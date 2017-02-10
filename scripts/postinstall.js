ab9cd9e0bf59c7ef2586ba4b6b28065f8b5223ce
Grigoriy Evdokimov <tapo4kii@gmail.com>
2017-02-10 18:09:18 +0300

    use streams instead of read/write file

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const hoganPath = require.resolve('hogan.js/dist/hogan-3.0.2.amd.js');
-const hoganAmd = fs.readFileSync(hoganPath);
-fs.writeFileSync('hogan-browser.js', hoganAmd);
+
+fs.createReadStream(hoganPath).pipe(fs.createWriteStream('hogan-browser.js'));
